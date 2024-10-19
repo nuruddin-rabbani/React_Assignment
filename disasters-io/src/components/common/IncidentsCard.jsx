ca458b1dad2bb8d396a0ab8a371cc5efabc8d929
nuruddin-rabbani <sadinbangla79@gmail.com>
2024-10-19 18:28:04 +0600

    Incidents Page stepper part responsive done

--- disasters-io/src/components/common/IncidentsCard.jsx
+++ disasters-io/src/components/common/IncidentsCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { cn } from "../../utilities/Index";
 function IncidentsCard({ img, title }) {
     return (
-        <button className="h-[69px] bg-[#e4e4e7] flex justify-center items-center rounded-[6px] gap-[7.5px]">
+        <button className="w-full lg:w-[180px] h-[50px] lg:h-[70px] bg-[#e4e4e7] flex justify-start ps-2.5 items-center rounded-[6px] gap-[7.5px]">
             <img className="size-6" src={img} alt="" />
-            <h5>{title}</h5>
+            <h5 className="text-[14px]">{title}</h5>
         </button>
     );
 }
--- disasters-io/src/components/sections/IncidentStep02.jsx
+++ disasters-io/src/components/sections/IncidentStep02.jsx
@@ -7,78 +7,83 @@ import React from "react";
 function IncidentStep02({ onNextStep }) {
     return (
         <section className="mt-[35px]">
-            <Container className="max-w-[753px] ">
+            <Container className="max-w-[800px] ">
                 <SectionTitle className="self-start text-[24px] md:text-[32px] font-bold leading-10 mb-[25px]">
                     Which of these best describes the incident?
                 </SectionTitle>
-                <div className="grid grid-cols-4 gap-[11px]">
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                     <IncidentsCard
                         img={"/img/Avalanche.png"}
                         title={"Avalanche"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Biological.png"}
+                        title={"Biological"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Blizzard.png"}
+                        title={"Blizzard"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/ColdFreezing.png"}
+                        title={"Cold/Freezing"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Drought.png"}
+                        title={"Drought"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Earthquake.png"}
+                        title={"Earthquake"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Flooding.png"}
+                        title={"Flooding"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/HeatWave.png"}
+                        title={"Heat Wave"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Hail.png"}
+                        title={"Hail"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Lightning.png"}
+                        title={"Lightning"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        className={"bg-primary"}
+                        img={"/img/incidentImg/ManMade.png"}
+                        title={"Man Made"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Mudslide.png"}
+                        title={"Mudslide"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/SevereStorm.png"}
+                        title={"Severe Storm"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/StrongWind.png"}
+                        title={"Strong Wind"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Tornado.png"}
+                        title={"Tornado"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/Tsunami.png"}
+                        title={"Tsunami"}
                     />
                     <IncidentsCard
-                        img={"/img/Avalanche.png"}
-                        title={"Avalanche"}
+                        img={"/img/incidentImg/VolcanicEruption.png"}
+                        title={"Volcanic Eruption"}
+                    />
+                    <IncidentsCard
+                        img={"/img/incidentImg/Wildfire.png"}
+                        title={"Wildfire"}
                     />
                 </div>
 
